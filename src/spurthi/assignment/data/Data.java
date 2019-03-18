27f11a394b403e5c8ce654e6dd83eee6216f25b6
spurthinaveli98 <46319198+spurthinaveli98@users.noreply.github.com>
2019-03-18 12:35:50 +0530

    Add files via upload

--- src/spurthi/assignment/data/Data.java
+++ src/spurthi/assignment/data/Data.java
@@ -2,30 +2,24 @@ package spurthi.assignment.data;
 
 public class Data {
     //Global variables
-    int global1;
-    char global2;
-
+    int collegeId;
+    char collegeName;
     /*
         Method for printing global variables.
      */
-    public void printGlobal()
-    {
-        System.out.println(global1);// prints 0
-        System.out.println(global2);//prints null
+    public void printGlobal() {
+        System.out.println(collegeId);// prints 0
+        System.out.println(collegeName);//prints null
     }
-
     /*
       Method for printing local variables.
       Compile time error, cannot print local variables with out initializing.
     */
-    public void printLocal()
-    {
+    public void printLocal() {
         //local variables
-        int local1;
-        char local2;
-        //System.out.println(local1);
-        //System.out.println(local2);
+        int studentId;
+        char studentName;
+        System.out.println(studentId);
+        System.out.println(studentName);
     }
-
-
 }
