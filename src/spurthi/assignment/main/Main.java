1a20ab247d79ab8c5ba456f1c7fbe90504c837e5
spurthinaveli98 <46319198+spurthinaveli98@users.noreply.github.com>
2019-03-18 13:07:42 +0530

    Add files via upload

--- src/spurthi/assignment/main/Main.java
+++ src/spurthi/assignment/main/Main.java
@@ -3,13 +3,19 @@ package spurthi.assignment.main;
 import spurthi.assignment.data.Data;
 import spurthi.assignment.singleton.Singleton;
 
-public class Main
-{
+public class Main {
     public static void main(String args[]) {
-        Data obj1= new Data();
-          obj1.printGlobal();
-          obj1.printLocal();
-        Singleton obj2=Singleton.getInstance("spurthi");
-          obj2.print();
+        Data data= new Data();
+          data.printGlobal();
+          data.printLocal();
+        Singleton singleton=Singleton.getInstance("spurthi");
+          singleton.print();
     }
 }
+
+/*
+-:Output:-
+0
+null
+spurthi
+ */
